4fa19ebdb5d223800ad26635a657b5e6bbd2a78d
Aaron Miller <millerac333@gmail.com>
2018-08-23 14:13:27 -0500

    filtered numbers in 2nd problem

--- Program.cs
+++ Program.cs
@@ -20,6 +20,19 @@ namespace linq
 
                 Console.WriteLine(item);
             }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            List<int> fourSixMultiples = numbers.Where(x => x % 4 == 0 || x % 6 == 0).ToList();
+            foreach (int number in fourSixMultiples)
+            {
+
+                Console.WriteLine(number);
+            }
         }
     }
 }
