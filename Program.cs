552e2a5e1d2e78784705cafc472b18059add019a
Aaron Miller <millerac333@gmail.com>
2018-08-23 15:29:45 -0500

    finished tasks 6 and 7

--- Program.cs
+++ Program.cs
@@ -67,6 +67,22 @@ namespace linq
                 15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
             };
             Console.WriteLine(allNumbers.Count());
+
+            // How much money have we made?
+            List<double> purchases = new List<double>()
+            {
+                2340.29, 745.31, 21.76, 34.03, 4786.45, 879.45, 9442.85, 2454.63, 45.65
+            };
+            //gives sum of numbers formatted to dollars 
+            Console.WriteLine($"{purchases.Sum().ToString("C")}");
+
+            // What is our most expensive product?
+            List<double> prices = new List<double>()
+            {
+                879.45, 9442.85, 2454.63, 45.65, 2340.29, 34.03, 4786.45, 745.31, 21.76
+            };
+            //gives most expensive item formatted to dollars 
+            Console.WriteLine($"{prices.Max().ToString("C")}");
         }
     }
 }
